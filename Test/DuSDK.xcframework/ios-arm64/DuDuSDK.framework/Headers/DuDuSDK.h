a3032b181d000d415e23b9bfde3c4c12edbb5c7c
yanws <yanws@nowosoft.cn>
2025-07-18 14:17:26 +0800

    update

---
+++ Test/DuSDK.xcframework/ios-arm64/DuDuSDK.framework/Headers/DuDuSDK.h
@@ -0,0 +1,27 @@
+//
+//  DuDuSDK.h
+//  DuDuSDK
+//
+//  Created by admin on 2025/7/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DuDuSDK.
+FOUNDATION_EXPORT double DuDuSDKVersionNumber;
+
+//! Project version string for DuDuSDK.
+FOUNDATION_EXPORT const unsigned char DuDuSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DuDuSDK/PublicHeader.h>
+
+@interface DuDuSDK : NSObject
+
++ (void)test01;
+
++ (void)test02;
+
++ (void)test03;
+
+@end
+
