899a848e4ef91ae8d75a17f4ad51c2ffe1cd0958
yanws <yanws@nowosoft.cn>
2025-07-18 15:25:42 +0800

    update

---
+++ Du01SDK.xcframework/ios-arm64/Du01SDK.framework/Headers/Du01SDK.h
@@ -0,0 +1,26 @@
+//
+//  Du01SDK.h
+//  Du01SDK
+//
+//  Created by admin on 2025/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Du01SDK.
+FOUNDATION_EXPORT double Du01SDKVersionNumber;
+
+//! Project version string for Du01SDK.
+FOUNDATION_EXPORT const unsigned char Du01SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Du01SDK/PublicHeader.h>
+
+@interface Du01SDK : NSObject
+
+
++ (void)test001;
+
++ (void)test002;
+
++ (void)test003;
+@end
